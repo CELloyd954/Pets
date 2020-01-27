1be2913e04491ebe1bb7a48cff18a4548efde4fd
Cayden Edward Lloyd <lloydce@s.dcsdk12.org>
2020-01-27 08:10:01 -0700

    changed horse

--- Horse.java
+++ Horse.java
@@ -1,11 +1,30 @@
 public class Horse extends Pet {
 
+	private boolean brushed;
+	private boolean ridden;
+
 	public Horse(String name) {
 		super(name);
 	}
 
+	public void brush() {
+		this.brushed = true;
+	}
+
+	public void ride() {
+		this.ridden = true;
+	}
+
+	public boolean getBrushed() {
+		return brushed;
+	}
+
+	public boolean getRidden() {
+		return ridden;
+	}
+
 	public boolean isHappy() {
-		return this.getFed() && this.getPetted() && this.getWalked() && this.getBrushed() && this.getRidden();
+		return this.getFed() && this.getPetted() && this.getWalked() && brushed && ridden;
 	}
 
 	public String talk() {
--- Pet.java
+++ Pet.java
@@ -5,19 +5,9 @@ public class Pet {
 	private boolean fed;
 	private boolean walked;
 	private boolean petted;
-	private boolean brushed;
-	private boolean ridden;
 
 	public Pet(String name) {
 		this.name = name;
-	}
-
-	public void brush() {
-		this.brushed = true;
-	}
-
-	public void ride() {
-		this.ridden = true;
 	}
 
 	public void feed() {
@@ -30,14 +20,6 @@ public class Pet {
 
 	public void pet() {
 		this.petted = true;
-	}
-
-	public boolean getBrushed() {
-		return brushed;
-	}
-
-	public boolean getRidden() {
-		return ridden;
 	}
 
 	public boolean getFed() {
